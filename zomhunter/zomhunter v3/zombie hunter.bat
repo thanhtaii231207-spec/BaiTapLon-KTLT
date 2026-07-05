3f5216dc90a785e3054199676183355289ba9723
thanhtaii231207-spec <thanhtaii231207@gmail.com>
2026-07-05 15:56:00 +0700

    Delete zomhunter directory

--- zomhunter/zomhunter v3/zombie hunter.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-g++ gametest.cpp -o gametest.exe -L. -lraylib -lopengl32 -lgdi32 -lwinmm
-if %errorlevel% equ 0 (
-    gametest.exe
-) else (
-    echo Bien dich that bai!
-)
-pause
--- zomhunter/zomhunter ver1/zombie hunter.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ gametest.cpp -o gametest -lraylib -lopengl32 -lgdi32 -lwinmm
-gametest.exe
-pause
